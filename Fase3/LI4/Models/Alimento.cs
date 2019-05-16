2cd95994cb2425d6fefd683f2e6a39de098fe7fb
FranciscoFreitas45 <zefreitas2011@hotmail.com>
2019-05-16 23:06:17 +0100

    fase3

---
+++ Fase3/LI4/Models/Alimento.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LI4.Models
+{
+    public class Alimento
+    {
+        private int id;
+        private float valorNutricional;
+        private DateTime validade;
+        private string nome;
+
+
+        public Alimento(int id, string nome, float valorNutricional, DateTime validade)
+        {
+            this.id = id;
+            this.SetNome(nome);
+            this.valorNutricional = valorNutricional;
+            this.SetValidade(validade);
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public void setId( int value)
+        {
+            this.id=value;
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public void SetNome(string value)
+        {
+            nome = value;
+        }
+
+        public DateTime GetValidade()
+        {
+            return validade;
+        }
+
+        public void SetValidade(DateTime value)
+        {
+            validade = value;
+        }
+
+        public float GetValorNutricional()
+        {
+            return valorNutricional;
+        }
+
+        public void SetValorNutricional(float value)
+        {
+            valorNutricional = value;
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            return obj is Alimento alimento &&
+                   id == alimento.id &&
+                   GetNome() == alimento.GetNome() &&
+                   valorNutricional == alimento.valorNutricional &&
+                   GetValidade() == alimento.GetValidade();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Fase3/LI4/Models/Utilizador.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LI4.Models
+{
+    public class Utilizador
+    {
+        private int id;
+        private string nome;
+        private DateTime data_nascimento;
+        private int username;
+        private string password;
+        private string email;
+        private string foto; // ?
+        private int admin;
+
+        public Utilizador(int id, string nome, DateTime data_nascimento, int username, string password, string email, string foto, int admin)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.data_nascimento = data_nascimento;
+            this.username = username;
+            this.password = password;
+            this.email = email;
+            this.foto = foto;
+            this.admin = admin;
+        }
+
+        public int GetUsername()
+        {
+            return username;
+        }
+
+        public void SetUsername(int value)
+        {
+            username = value;
+        }
+
+        public string GetPassword()
+        {
+            return password;
+        }
+
+        public void SetPassword(string value)
+        {
+            password = value;
+        }
+
+        public string GetEmail()
+        {
+            return email;
+        }
+
+        public void SetEmail(string value)
+        {
+            email = value;
+        }
+
+        public string GetFoto()
+        {
+            return foto;
+        }
+
+        public void SetFoto(string value)
+        {
+            foto = value;
+        }
+
+        public int GetAdmin()
+        {
+            return admin;
+        }
+
+        public void SetAdmin(int value)
+        {
+            admin = value;
+        }
+
+        public int GetId()
+        {
+            return id;
+        }
+
+        public void SetId(int value)
+        {
+            id = value;
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public void SetNome(string value)
+        {
+            nome = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Utilizador utilizador &&
+                   id == utilizador.id &&
+                   nome == utilizador.nome &&
+                   data_nascimento == utilizador.data_nascimento &&
+                   username == utilizador.username &&
+                   password == utilizador.password &&
+                   email == utilizador.email &&
+                   foto == utilizador.foto &&
+                   admin == utilizador.admin;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
