78be5d88893407e2073d4c5dd3fa250425851638
Inês <inesamorimalves@gmail.com>
2019-05-27 21:28:37 +0100

    DAO receita, verificar timeSpan vs int

---
+++ JARVIS/Data Access/Receita.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace JARVIS.DataAccess
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Receita
+    {
+        public Receita(int id, String nome, String descricao, TimeSpan duracao)
+        {
+            idReceita = id;
+            Nome = nome;
+            Descricao = descricao;
+            Duracao = duracao;
+        }
+
+        public Receita() { }
+
+        public int idReceita { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+        public TimeSpan Duracao { get; set; }
+    }
+}
---
+++ JARVIS/Data Access/ReceitaDAO.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace JARVIS.DataAccess
+{
+    public class ReceitaDAO : IDAO<Receita>
+    {
+        private IConnection _connection;
+
+        public ReceitaDAO(IConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public Receita FindById (string key)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public Receita Insert(Receita obj)
+        {
+            //SqCommand command ...
+            using (SqlCommand command = _connection.Fetch().CreateCommand())
+            {
+                command.CommandType = CommandType.Text;
+                command.CommandText = "INSERT INTO Receita values (@idReceita,@Nome,@Descricao,@Duracao)";
+
+                command.Parameters.Add("@idReceita", SqlDbType.Int).Value = obj.idReceita;
+                command.Parameters.Add("@Nome", SqlDbType.Text).Value = obj.Nome;
+                command.Parameters.Add("@Descricao", SqlDbType.Text).Value = obj.Descricao;
+                command.Parameters.Add("@Duracao", SqlDbType.Time).Value = obj.Duracao;
+
+                command.ExecuteNonQuery();
+                // obj.Id = command.ExecuteScalar().ToString(); -> devolve o valor da primeira linha e primeira coluna da tabela em questão
+                //
+            }
+            return obj;
+        }
+
+        public Collection<Receita> ListAll()
+        {
+            Collection<Receita> receitas = new Collection<Receita>();
+
+            using (SqlCommand command = _connection.Fetch().CreateCommand())
+            {
+                command.CommandType = CommandType.Text;
+                command.CommandText = "SELECT idReceita, Nome, Descricao, Duracao";
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                {
+                    DataTable tab = new DataTable();
+                    adapter.Fill(tab);
+
+                    foreach (DataRow row in tab.Rows)
+                    {
+                        Receita r = new Receita
+                        {
+                            idReceita = int.Parse(row["idReceita"].ToString()),
+                            Nome = row["Nome"].ToString(),
+                            Descricao = row["Descricao"].ToString(),
+                            Duracao = TimeSpan.Parse(row["Duracao"].ToString())
+                        };
+                        receitas.Add(r);
+                    }
+                }
+                return receitas;
+            }
+        }
+
+        public bool remove(Receita obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Receita obj)
+        {
+            bool updated = false;
+            using (SqlCommand command = _connection.Fetch().CreateCommand())
+            {
+                command.CommandType = CommandType.Text;
+                command.CommandText = "UPDATE Receita Set Duracao==Duracao WHERE idReceita==idReceita";
+
+                command.Parameters.Add("@Duracao", SqlDbType.Decimal).Value = obj.Duracao;
+                command.Parameters.Add("@idReceita", SqlDbType.Int).Value = obj.idReceita;
+
+                if (command.ExecuteNonQuery() > 0)
+                {
+                    updated = true;
+                }
+            }
+            return updated;
+        }
+
+    }
+}
