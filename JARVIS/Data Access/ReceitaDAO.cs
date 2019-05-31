f62150ffa69ec48bca581177ae594b476666da41
Pedro Freitas <pedrofreitas@MBP-de-Pedro.home>
2019-05-31 23:46:34 +0100

    Pegar nos alimentos(PS: Era necessário visitar duas tabelas, como não sei muito bem usar inner joins, fiz de uma maneira diferente, mas vou mudar isto)

--- JARVIS/Data Access/ReceitaDAO.cs
+++ JARVIS/Data Access/ReceitaDAO.cs
@@ -21,22 +21,25 @@ namespace JARVIS.DataAccess
             throw new NotImplementedException();
         }
 
+
+
+
+
         public Receita Insert(Receita obj)
         {
-            //SqCommand command ...
-            using (SqlCommand command = _connection.Fetch().CreateCommand())
+            using (SqlConnection con = _connection.Fetch())
             {
-                command.CommandType = CommandType.Text;
-                command.CommandText = "INSERT INTO Receita values (@idReceita,@Nome,@Descricao,@Duracao)";
+                String query = "INSERT INTO dbo.Utilizador(Nome,Descricao,Duracao) values (@Nome,@Descricao,@Duracao)";
 
-                command.Parameters.Add("@idReceita", SqlDbType.Int).Value = obj.idReceita;
-                command.Parameters.Add("@Nome", SqlDbType.Text).Value = obj.Nome;
-                command.Parameters.Add("@Descricao", SqlDbType.Text).Value = obj.Descricao;
-                command.Parameters.Add("@Duracao", SqlDbType.Time).Value = obj.Duracao;
+                using (SqlCommand command = new SqlCommand(query, con))
+                {
+                    command.Parameters.Add("@Nome", SqlDbType.VarChar).Value = obj.Nome ?? (object)DBNull.Value;
+                    command.Parameters.Add("@Descricao", SqlDbType.Date).Value = obj.Descricao;
+                    command.Parameters.Add("@Duracao", SqlDbType.VarChar).Value = obj.Duracao;
 
-                command.ExecuteNonQuery();
-                // obj.Id = command.ExecuteScalar().ToString(); -> devolve o valor da primeira linha e primeira coluna da tabela em questão
-                //
+                    command.ExecuteNonQuery();
+
+                }
             }
             return obj;
         }
@@ -76,6 +79,58 @@ namespace JARVIS.DataAccess
 
 
 
+        //PARA JÁ VOU SELECIONAR TODA A TABELA DE UM ALIMENTO E FAZER COM ROWS DEPOIS TENHO DE ALTERAR
+        public Collection<Alimento> ListaAlimento(int id) 
+        {
+            Collection<Alimento> alimentos = new Collection<Alimento>();
+            int idAl;
+
+            using (SqlConnection con = _connection.Fetch())
+            {
+                string queryString = "SELECT Alimento_idAlimento FROM Receita_Alimento WHERE idReceita = id";
+                string queryString2 = "SELECT * FROM Alimento where idAlimento = idAl";
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter())
+                {
+                    adapter.SelectCommand = new SqlCommand(queryString, con);
+                    DataSet tab = new DataSet();
+                    adapter.Fill(tab);
+                    foreach (DataTable table in tab.Tables)
+                    {
+                        foreach (DataRow row in table.Rows)
+                        {
+                            idAl = int.Parse(row["idAlimento"].ToString());
+                            using (SqlDataAdapter adapter2 = new SqlDataAdapter())
+                            {
+                                adapter2.SelectCommand = new SqlCommand(queryString2, con);
+                                DataSet tab2 = new DataSet();
+                                adapter.Fill(tab2);
+
+                                foreach (DataTable table2 in tab2.Tables)
+                                {
+                                    foreach (DataRow row2 in table2.Rows)
+                                    {
+                                        Alimento a = new Alimento
+                                        {
+                                            idAlimento = int.Parse(row["idAlimento"].ToString()),
+                                            Nome = row["Nome"].ToString(),
+                                            ValorNutricional = double.Parse(row["ValorNutricional"].ToString()),
+                                            Validade = DateTime.Parse(row["Validade"].ToString())
+
+                                        };
+                                        alimentos.Add(a);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return alimentos;
+        }
+
+
+
 
 
 
@@ -84,20 +139,26 @@ namespace JARVIS.DataAccess
             throw new NotImplementedException();
         }
 
+
+
         public bool Update(string key, Receita obj)
         {
             bool updated = false;
-            using (SqlCommand command = _connection.Fetch().CreateCommand())
+            using (SqlConnection con = _connection.Fetch())
             {
-                command.CommandType = CommandType.Text;
-                command.CommandText = "UPDATE Receita Set Duracao==Duracao WHERE idReceita==idReceita";
+                String query = "UPDATE dbo.Receita SET Nome=@Nome, Descricao=@Descricao, Duracao=@Duracao WHERE idUtilizador=@idUtilizador ";
 
-                command.Parameters.Add("@Duracao", SqlDbType.Decimal).Value = obj.Duracao;
-                command.Parameters.Add("@idReceita", SqlDbType.Int).Value = obj.idReceita;
 
-                if (command.ExecuteNonQuery() > 0)
+                using (SqlCommand command = new SqlCommand(query, con))
                 {
-                    updated = true;
+                    command.Parameters.AddWithValue("@Nome", obj.Nome);
+                    command.Parameters.AddWithValue("@Descricao", obj.Descricao);
+                    command.Parameters.AddWithValue("@Duracao", obj.Duracao);
+
+                    if (command.ExecuteNonQuery() > 0)
+                    {
+                        updated = true;
+                    }
                 }
             }
             return updated;
